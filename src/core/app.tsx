52699502200ff8564095f4d799178b45b3a6a78c
Codeco <adrielcodeco@hotmail.com>
2023-02-05 19:34:25 -0300

    feat: obb cert

--- src/core/app.tsx
+++ src/core/app.tsx
@@ -5,6 +5,7 @@ import { Routes, Route } from 'react-router-dom'
 import { HomePage } from 'src/pages/home'
 import { OBInformPage } from 'src/pages/ob-inform'
 import { OpenidCertPage } from 'src/pages/openid-cert'
+import { OBBCertPage } from 'src/pages/obb-cert'
 
 type AppProps = {}
 
@@ -18,7 +19,7 @@ export const App = (props: AppProps) => {
     <Routes>
       <Route path="/open-banking/informes" element={<OBInformPage />} />
       <Route path="/open-banking/timeline" element={<HomePage />} />
-      <Route path="/open-banking/certification" element={<HomePage />} />
+      <Route path="/open-banking/certification" element={<OBBCertPage />} />
       <Route path="/open-insurance/informes" element={<HomePage />} />
       <Route path="/open-insurance/timeline" element={<HomePage />} />
       <Route path="/open-insurance/certification" element={<HomePage />} />
---
+++ src/pages/obb-cert.tsx
@@ -0,0 +1,4 @@
+/** @jsxImportSource @emotion/react */
+import { OBBCert } from 'src/components/templates/obbCert'
+
+export const OBBCertPage = () => <OBBCert />
